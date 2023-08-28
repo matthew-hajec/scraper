cab83c777bfe66e0b9726c89c47ff1a9cae32ed9
matthew-hajec <matthew.hajec02@gmail.com>
2023-08-28 14:28:55 -0400

    deleted unnecessary api error

--- src/external_data/errors.py
+++ src/external_data/errors.py
@@ -1,8 +1,3 @@
 class MalformedContent(Exception):
     """Exception raised when a request is successful but did not return what was expected"""
     pass
-
-
-class ExceededMaxFailures(Exception):
-    """Exception raised when an external data source fails too many times."""
-    pass
